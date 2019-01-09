f4ae68e614aa75df427617bcaf92c79a864b6c06
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-09 15:33:39 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,10 @@
 def using_while
-	levitation_force = 6
+	levitation_force = 1
 
 puts "Wingardium Leviosa"
+puts "Wingardium Leviosa"
+puts "Wingardium Leviosa"
+puts "Wingardium Leviosa"
+puts "Wingardium Leviosa"
+
 end
